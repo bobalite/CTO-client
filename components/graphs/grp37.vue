2bb5c442e83a7bd4418a7b4212e4719d809ff1d8
bobalite <bobalite@gmail.com>
2025-06-25 09:06:22 +0800

    test

--- components/graphs/grp37.vue
+++ components/graphs/grp37.vue
@@ -128,7 +128,7 @@ function fetchReports_Details_Actuals() { // main fetching function for actuals
         const report_schedule = state.report_details.data[0].report_schedule
         console.log('report_schedule', report_schedule)
         console.log('props.report_year' ,props.report_year )
-        if (report_schedule == 'Annualy'){
+      
             for (const c in props.passed_year_data.data) {
                 if (props.passed_year_data.data[c].id == props.report_year) {
                     state.report_year = props.passed_year_data.data[c].year
@@ -150,20 +150,7 @@ function fetchReports_Details_Actuals() { // main fetching function for actuals
                     state.graphSeriesFemale[2] = parseFloat(state.graphSeriesFemale[2]) + parseFloat(state.report_details.data[c].female)
                 }
             }
-        }else{
-            for (const c in state.report_details.data) {
-                if (state.report_details.data[c].sequence_header == '2.3.1' && state.report_details.data[c].entry_type == 'Actual' && state.report_details.data[c].report_year_id == props.report_year) {
-                    state.graphSeriesMale[0] = parseFloat(state.graphSeriesMale[0]) + parseFloat(state.report_details.data[c].male)
-                    state.graphSeriesFemale[0] = parseFloat(state.graphSeriesFemale[0]) + parseFloat(state.report_details.data[c].female)
-                } else if (state.report_details.data[c].sequence_header == '2.3.2' && state.report_details.data[c].entry_type == 'Actual' && state.report_details.data[c].report_year_id == props.report_year) {
-                    state.graphSeriesMale[1] = parseFloat(state.graphSeriesMale[1]) + parseFloat(state.report_details.data[c].male)
-                    state.graphSeriesFemale[1] = parseFloat(state.graphSeriesFemale[1]) + parseFloat(state.report_details.data[c].female)
-                } else if (state.report_details.data[c].sequence_header == '2.3.3' && state.report_details.data[c].entry_type == 'Actual' && state.report_details.data[c].report_year_id == props.report_year) {
-                    state.graphSeriesMale[2] = parseFloat(state.graphSeriesMale[2]) + parseFloat(state.report_details.data[c].male)
-                    state.graphSeriesFemale[2] = parseFloat(state.graphSeriesFemale[2]) + parseFloat(state.report_details.data[c].female)
-                }
-            }
-        }
+   
         state.graphSeriesAll[0] = { name: "Male", data: state.graphSeriesMale };
         state.graphSeriesAll[1] = { name: "Female", data: state.graphSeriesFemale };
     } catch (error) {
